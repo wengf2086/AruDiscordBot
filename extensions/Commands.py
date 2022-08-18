960ac671b6d74f6f1a6c0baaf2c89ea92457e6cf
Frank Weng <78763842+wengf2086@users.noreply.github.com>
2022-08-17 21:26:33 -0400

    Added reactbomb functionality

--- extensions/Commands.py
+++ extensions/Commands.py
@@ -1,17 +1,28 @@
+from asyncio.windows_events import NULL
 import hikari
 import lightbulb
+import re
 
 plugin = lightbulb.Plugin('Commands')
 
 # Command functions
-# /ping - Aru responds with 'Pong!'
-@plugin.command
-@lightbulb.command('ping', 'Says pong!')
-@lightbulb.implements(lightbulb.SlashCommand)
-async def ping(ctx):
-    await ctx.respond('Pong!')
 
-# /ping - Aru responds with 'Pong!'
+@plugin.command
+@lightbulb.option('emojis', 'emojis to be used as reactions. Only default emojis and emojis from this server are allowed.', type = str)
+@lightbulb.option('message_id', 'ID of the message to be bombed')
+@lightbulb.option('channel', 'ID of the channel of the message to be bombed')
+@lightbulb.command('reactbomb', '\'Bombs\' a message with a bunch of (MAX: 20) reactions!')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def reactbomb(ctx):
+    channel_str = re.split(r'<#|>', ctx.options.channel)
+    channel = channel_str[0] if (len(channel_str) == 1) else channel_str[1]
+
+    for emoji_str in ctx.options.emojis.split(" "):
+        emoji = hikari.Emoji.parse(emoji_str)
+        await plugin.app.rest.add_reaction(channel = channel, message = ctx.options.message_id, emoji = emoji)
+                
+
+
 @plugin.command
 @lightbulb.command('group', 'This is a group')
 @lightbulb.implements(lightbulb.SlashCommandGroup)
@@ -25,8 +36,8 @@ async def subcommand(ctx):
     await ctx.respond('I am a subcommand!')
 
 @plugin.command
-@lightbulb.option('num2', 'The second number', type=int)
-@lightbulb.option('num1', 'The first number', type=int)
+@lightbulb.option('str2', 'The second number', type=int)
+@lightbulb.option('str1', 'The first number', type=int)
 @lightbulb.command('add', 'Add two numbers together')
 @lightbulb.implements(lightbulb.SlashCommand)
 async def add(ctx):
