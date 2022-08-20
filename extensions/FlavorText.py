0d53be76bee5a738213f727b2f604c7a863f791e
Frank Weng <78763842+wengf2086@users.noreply.github.com>
2022-08-19 23:29:28 -0400

    Updated Commands and FlavorText

--- extensions/FlavorText.py
+++ extensions/FlavorText.py
@@ -5,6 +5,7 @@ import datetime
 
 plugin = lightbulb.Plugin('FlavorText')
 silentMode = False # if True, plugin will not load.
+flavorTextChannel = 1009180448003465316
 
 # Event functions
 # When a message is sent in a guild, print the content associated with it (in the console).
@@ -28,12 +29,12 @@ async def start_bot(event):
     elif int(currTime[0:2]) < 6:
         addText = " It's late... Should you be up? I hope you can get some sleep soon."
 
-    await plugin.app.rest.create_message(channel = 1009180448003465316, content = f"Good Morning! It is now {currTime}!{addText}")
+    await plugin.app.rest.create_message(channel = flavorTextChannel, content = f"Good Morning! It is now {currTime}!{addText}")
 
 # When the bot is stopped
 @plugin.listener(hikari.StoppingEvent)
 async def start_bot(event):
-    await plugin.app.rest.create_message(channel = 1009180448003465316, content = f"_Getting... sleepy..._ (Aru is now offline.)")
+    await plugin.app.rest.create_message(channel = flavorTextChannel, content = f"_Getting... sleepy..._ (Aru is now offline.)")
 
 def load(bot):
     if not silentMode:
